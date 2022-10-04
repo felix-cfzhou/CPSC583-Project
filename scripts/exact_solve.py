28065fc40ad11998099b7f92aceeebf58eb12106
Felix Zhou <felix.zhou@yale.edu>
2022-10-04 19:16:24 -0400

    edit config file to fix path errors

--- scripts/exact_solve.py
+++ scripts/exact_solve.py
@@ -1,4 +1,4 @@
 if __name__ == "__main__":
   for graph in range(1, 201):
-    task = f"module load GCC; /gpfs/gibbs/project/karbasi/cz397/project/CPSC583-Project/KaMIS/deploy/online_mis /gpfs/gibbs/project/karbasi/cz397/project/CPSC583-Project/pace2019_track1_vc_exact_all/vc-exact_{graph:03}.graph --time_limit=36000 --output=/gpfs/gibbs/project/karbasi/cz397/project/CPSC583-Project/exact_solve/vc-exact_{graph:03}.ind"
+    task = f"module load GCC; /home/cz397/project/CPSC583-Project/KaMIS/deploy/online_mis /home/cz397/project/CPSC583-Project/pace2019_track1_vc_exact_all/vc-exact_{graph:03}.graph --time_limit=36000 --output=/home/cz397/project/CPSC583-Project/exact_solve/vc-exact_{graph:03}.ind"
     print(task)
