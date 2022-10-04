e66992736029f2a6c2fca8fbb0610d56abf37d26
Felix Zhou <felix.zhou@yale.edu>
2022-10-04 00:41:14 -0400

    job script uses exact paths

--- scripts/dsq-exact_solve-2022-10-04.sh
+++ scripts/dsq-exact_solve-2022-10-04.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --array 0-199%2000
 #SBATCH --output dsq-exact_solve-%A_%3a-%N.out
+#SBATCH --array 0-199%2000
 #SBATCH --job-name dsq-exact_solve
-#SBATCH --cpus-per-task=16 --mem-per-cpu=18G -t 12:00:00 --mail-type ALL
+#SBATCH --cpus-per-task=16 --mem-per-cpu=8G -t 12:00:00 --mail-type ALL
 
 # DO NOT EDIT LINE BELOW
-/gpfs/loomis/apps/avx/software/dSQ/1.05/dSQBatch.py --job-file /gpfs/gibbs/project/karbasi/cz397/CPSC583-Project/scripts/exact_solve.txt --status-dir /gpfs/gibbs/project/karbasi/cz397/CPSC583-Project/
+/gpfs/loomis/apps/avx/software/dSQ/1.05/dSQBatch.py --job-file /gpfs/gibbs/project/karbasi/cz397/CPSC583-Project/scripts/exact_solve.txt --status-dir /gpfs/gibbs/project/karbasi/cz397/CPSC583-Project/scripts
 
