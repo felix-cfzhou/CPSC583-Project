f64934be4609529f84aaf820696d7656bdbbb5ac
Felix Zhou <felix.zhou@yale.edu>
2022-10-09 18:31:33 -0400

    scripts to generate exact solutions with KaMIS

---
+++ scripts/dsq-pace_weighted_0-2022-10-09.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --output dsq-pace_weighted_0-%A_%4a-%N.out
+#SBATCH --array 0-9999%2000
+#SBATCH --job-name dsq-pace_weighted_0
+#SBATCH --cpus-per-task=1 --mem-per-cpu=16G -t 7:00:00 --mail-type ALL
+
+# DO NOT EDIT LINE BELOW
+/gpfs/loomis/apps/avx/software/dSQ/1.05/dSQBatch.py --job-file /gpfs/gibbs/project/karbasi/cz397/CPSC583-Project/scripts/pace_weighted_0.txt --status-dir /gpfs/gibbs/project/karbasi/cz397/CPSC583-Project/out/dsq-pace_weighted_1-2022-10-09/
+
--- scripts/gen_pace_weighted.sh
+++ scripts/gen_pace_weighted.sh
@@ -2,9 +2,9 @@
 
 #SBATCH --output gen_pace_weighted.out
 #SBATCH --job-name=gen_pace_weighted
-#SBATCH --cpus-per-task=1 --mem-per-cpu=8G -t 6:00:00 --mail-type ALL
+#SBATCH --cpus-per-task=1 --mem-per-cpu=1G -t 6:00:00 --mail-type ALL
 
 module load SciPy-bundle
 module load networkx
 
-python3 /home/cz397/project/CPSC583-Project/wmis_gnn_py/pace2019_to_metis.py /home/cz397/project/CPSC583-Project/data/pace2019_track1_vc_exact_all/ --samples_per_graph=500 --random_state=2019 --output_dir=/home/cz397/project/CPSC583-Project/data/pace_weighted/
+python3 /home/cz397/project/CPSC583-Project/wmis_gnn_py/pace2019_to_metis.py /home/cz397/project/CPSC583-Project/data/pace2019_track1_vc_exact_all/ --samples_per_graph=100 --random_state=2019 --output_dir=/home/cz397/project/CPSC583-Project/data/pace_weighted/
