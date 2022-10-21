3fd07f313d4c9262dd6008c7d7fddff9ad1eda7c
Felix Zhou <felix.zhou@yale.edu>
2022-10-21 15:17:08 -0400

    implemented interface with Gurobi for WMIS LP

--- wmis_gnn_py/util/gurobi_wmis.py
+++ wmis_gnn_py/util/gurobi_wmis.py
@@ -29,3 +29,38 @@ def wmis_ilp(G: nx.Graph):
         print(f"Error code {e.errno}: {e}")
     except AttributeError:
         print("Encountered an attribute error")
+
+
+def wmis_lp(G: nx.Graph):
+    try:
+        m = gp.Model("Weighted Maximum Independent Set")
+
+        m.setAttr("ModelSense", -1)
+
+        nodes = list(range(1, G.order() + 1))
+        node_weights = nx.get_node_attributes(G, "weight")
+
+        x = m.addVars(
+            nodes,
+            lb=0.0,
+            ub=1.0,
+            obj=node_weights,
+            vtype=GRB.CONTINUOUS,
+            name="vertices",
+        )
+
+        m.addConstrs((x[u] + x[v] <= 1 for u, v in G.edges), "cap")
+
+        m.optimize()
+
+        if m.status == GRB.OPTIMAL:
+            solution = m.getAttr("x", x)
+            solution = {k: v for k, v in solution.items()}
+            nx.set_node_attributes(G, solution, name="solution")
+        else:
+            raise Exception("Did not found optimal solution!")
+
+    except gp.GurobiError as e:
+        print(f"Error code {e.errno}: {e}")
+    except AttributeError:
+        print("Encountered an attribute error")
