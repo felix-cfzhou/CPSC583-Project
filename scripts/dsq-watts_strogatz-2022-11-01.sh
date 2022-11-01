bf9f57d848875c71fa1b75123073cc68470b3237
Felix Zhou <felix.zhou@yale.edu>
2022-11-01 09:17:58 -0400

    generate new watts_strogatz data with higher p_edge

--- scripts/dsq-watts_strogatz-2022-11-01.sh
+++ scripts/dsq-watts_strogatz-2022-11-01.sh
@@ -5,5 +5,5 @@
 #SBATCH --cpus-per-task=1 --mem-per-cpu=64G -t 7:00:00 --mail-type ALL
 
 # DO NOT EDIT LINE BELOW
-/gpfs/loomis/apps/avx/software/dSQ/1.05/dSQBatch.py --job-file /gpfs/gibbs/project/karbasi/cz397/CPSC583-Project/scripts/watts_strogatz.txt --status-dir /gpfs/gibbs/project/karbasi/cz397/CPSC583-Project/scripts
+/gpfs/loomis/apps/avx/software/dSQ/1.05/dSQBatch.py --job-file /gpfs/gibbs/project/karbasi/cz397/CPSC583-Project/scripts/watts_strogatz.txt --status-dir /home/cz397/project/CPSC583-Project/out/watts_strogatz
 
--- scripts/gen_watts_strogatz.sh
+++ scripts/gen_watts_strogatz.sh
@@ -2,9 +2,9 @@
 
 #SBATCH --output gen_watts_strogatz.out
 #SBATCH --job-name=gen_watts_strogatz
-#SBATCH --cpus-per-task=1 --mem-per-cpu=8G -t 4:00:00 --mail-type ALL
+#SBATCH --cpus-per-task=1 --mem-per-cpu=2G -t 1:00:00 --mail-type ALL
 
 module load SciPy-bundle
 module load networkx
 
-python3 /home/cz397/project/CPSC583-Project/wmis_gnn_py/gen_watts_strogatz.py --n_graphs=10000 --n_vertices=1000 --k_neighbors=7 --p_edge=0.5 --random_seed=5000 --output_dir=/home/cz397/project/CPSC583-Project/data/watts_strogatz/
+python3 /home/cz397/project/CPSC583-Project/wmis_gnn_py/gen_watts_strogatz.py --n_graphs=10000 --n_vertices=1000 --k_neighbors=7 --p_edge=0.9 --random_seed=5000 --output_dir=/home/cz397/project/CPSC583-Project/data/watts_strogatz/
