5e8c7d87e69c9ebc29e830372e9a2294b81f2570
Felix Zhou <felix.zhou@yale.edu>
2022-11-01 14:56:32 -0400

    re-generated watts-strogatz data for k=5 neighbors

--- scripts/gen_watts_strogatz.sh
+++ scripts/gen_watts_strogatz.sh
@@ -7,4 +7,4 @@
 module load SciPy-bundle
 module load networkx
 
-python3 /home/cz397/project/CPSC583-Project/wmis_gnn_py/gen_watts_strogatz.py --n_graphs=10000 --n_vertices=1000 --k_neighbors=7 --p_edge=0.9 --random_seed=5000 --output_dir=/home/cz397/project/CPSC583-Project/data/watts_strogatz/
+python3 /home/cz397/project/CPSC583-Project/wmis_gnn_py/gen_watts_strogatz.py --n_graphs=10000 --n_vertices=1000 --k_neighbors=5 --p_edge=0.9 --random_seed=5000 --output_dir=/home/cz397/project/CPSC583-Project/data/watts_strogatz/
