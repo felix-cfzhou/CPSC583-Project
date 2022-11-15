4393d83a4c89212a684d48c1279e01411ea4a0b3
Felix Zhou <felix.zhou@yale.edu>
2022-11-15 13:37:24 -0500

    run local search on datasets

---
+++ scripts/dsq-local_search_ws-2022-11-15.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --output dsq-local_search_ws-%A_%4a-%N.out
+#SBATCH --array 0-9999%2000
+#SBATCH --job-name dsq-local_search_ws
+#SBATCH --cpus-per-task=1 --mem-per-cpu=2G -t 1:00:00 --mail-type ALL
+
+# DO NOT EDIT LINE BELOW
+/gpfs/loomis/apps/avx/software/dSQ/1.05/dSQBatch.py --job-file /gpfs/gibbs/project/karbasi/cz397/CPSC583-Project/scripts/local_search_ws.txt --status-dir /home/cz397/project/CPSC583-Project/out/local_search_ws
+
